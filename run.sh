e72d600279a3c06ed96d235137b0bfeec21c96ef
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-08-18 10:10:35 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,27 @@
-# Executar stack sock-shop:
-
-kubectl create -f microservice-demo-weaveworks-socks.yaml
-kubectl get svc -n sock-shop
-
-# Executar chat:
-
-kubectl create -f chat.yaml
-kubectl get svc -n fiap-chat
+# Escolher se quer executar no modo NODEPORT ou LOADBALANCER
+echo "Digite 1 ou 2 para escolher se quer executar no modo NODEPORT ou LOADBALANCE:"
+echo "1) NODEPORT"  
+echo "2) LOADBALANCE"
+read MODO
+if [[ "$MODO" -eq 1 ]]; then
+    echo "NODEPORT:"
+    # Executar stack modo NODEPORT:
+    kubectl create -f microservice-demo-weaveworks-socks.yaml
+    kubectl get svc -n sock-shop
+    kubectl create -f chat_deploy_svc.yml
+    kubectl get svc -n fiap-chat
+    kubectl create -f deploy_fiap.yml
+    kubectl create -f svc_fiap.yml
+    kubectl get svc -n fiap-service  
+elif [[ "$MODO" -eq 2 ]]; then
+    echo "LOADBALANCE:"
+    # Executar stack modo LOADBALANCE:
+    kubectl create -f microservice-demo-weaveworks-socks.yaml
+    kubectl get svc -n sock-shop
+    kubectl create -f chat_deploy_svc.yml
+    kubectl get svc -n fiap-chat
+    kubectl create -f deploy_fiap.yml
+    kubectl create -f svc_fiap_gcp.yml
+    kubectl get svc -n fiap-service      
+else echo "Voce precisa escolher 1 ou 2."
+fi
