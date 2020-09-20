cb777cce6446680e7cb248f4094d58e48c0cea9c
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-09-20 17:35:45 -0300

    Update config_cluster_from_cloud9.sh

--- config_cluster_from_cloud9.sh
+++ config_cluster_from_cloud9.sh
@@ -7,7 +7,26 @@ NODE1=$(~/environment/ip | awk -Fv '/vm_1/{print $1}')
 NODE2=$(~/environment/ip | awk -Fv '/vm_2/{print $1}')
 NODE3=$(~/environment/ip | awk -Fv '/vm_3/{print $1}')
 
+# CONFIGURANDO O MASTER utilizando o KUBEADM INIT:
+echo "sudo hostnamectl set-hostname master" >> master.sh
+echo "kubeadm version" >> master.sh
+echo "sudo kubeadm config images pull" >> master.sh
+echo "sudo kubeadm init" >> master.sh
+#	Configurar o cliente kubectl:
+echo "mkdir -p $HOME/.kube" >> master.sh
+echo "sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config" >> master.sh
+echo "sudo chown $(id -u):$(id -g) $HOME/.kube/config" >> master.sh
+#echo "source <(kubectl completion bash)" >> master.sh
+
+# validar
+#cat master.sh
+
+# Configurar via SSH
 ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@MASTER 'bash -s' < config_master.sh
+# Configurar a rede do cluster:
+ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@MASTER 'bash -s' < config_network_weave.sh
+
+### CONFIGURANDO OS NODES utilizando o KUBEADM JOIN:
 
 # Exemplo:
 # kubeadm join 10.0.1.169:6443 --token fdwf9o.om0jvrom7uv3eeg4 \
@@ -25,8 +44,11 @@ echo "sudo $TOKEN $ENTER" >> worker2.sh
 echo "sudo $TOKEN $ENTER" >> worker3.sh
 
 # validar
-cat worker1.sh
+#cat worker1.sh
 
 ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$NODE1 'bash -s' < worker1.sh
 ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$NODE2 'bash -s' < worker2.sh
 ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$NODE3 'bash -s' < worker3.sh
+
+### CONFIGURANDO OS VOLUMES 
+ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@MASTER 'bash -s' < config_volume_portworx.sh
