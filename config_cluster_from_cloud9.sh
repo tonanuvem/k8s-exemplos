cf2cea8c9ad2c52ccaf23c6bbeba8936ebe8b784
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-09-20 16:54:46 -0300

    Update config_cluster_from_cloud9.sh

--- config_cluster_from_cloud9.sh
+++ config_cluster_from_cloud9.sh
@@ -8,3 +8,14 @@ NODE2=$(~/environment/ip # | awk -Fv '/vm_2/{print $1}')
 NODE3=$(~/environment/ip # | awk -Fv '/vm_3/{print $1}')
 
 ssh -i ~/environment/chave-fiap.pem ubuntu@MASTER 'bash -s' < config_master.sh
+
+echo "Copie e cole o JOIN TOKEN exibido acima: (digite ENTER PARA CONCLUIR)"
+read TOKEN_LINHA1
+read TOKEN_LINHA2
+read ENTER
+
+echo "sudo $TOKEN_LINHA1 $TOKEN_LINHA2 $ENTER" >> token.sh
+
+ssh -i ~/environment/chave-fiap.pem ubuntu@NODE1 'bash -s' < token.sh
+ssh -i ~/environment/chave-fiap.pem ubuntu@NODE2 'bash -s' < token.sh
+ssh -i ~/environment/chave-fiap.pem ubuntu@NODE3 'bash -s' < token.sh
