2887137dc3daa537a2107934921528946b1efe96
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-09-20 16:14:57 -0300

    Create config_master.sh

---
+++ config_master.sh
@@ -0,0 +1,18 @@
+# Inicializar o Master utilizando o KUBEADM:
+
+kubeadm version
+
+sudo kubeadm config images pull
+
+sudo kubeadm init
+
+#	Configurar o cliente kubectl:
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+source <(kubectl completion bash)
+
+# Configurar a rede:
+
+sh config_network_weave.sh
