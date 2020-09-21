fc73079beac25a4bec63a002049b1cd8741eb3b7
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-09-20 23:21:24 -0300

    Update config_cluster_from_cloud9.sh

--- config_cluster_from_cloud9.sh
+++ config_cluster_from_cloud9.sh
@@ -6,11 +6,11 @@ MASTER=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_0/) ) { prin
 NODE1=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_1/) ) { print $1} }')
 NODE2=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_2/) ) { print $1} }')
 NODE3=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_3/) ) { print $1} }')
-echo "IPs :"
-echo $MASTER
-echo $NODE1
-echo $NODE2
-echo $NODE3
+echo "IPs configurados :"
+echo "MASTER = $MASTER"
+echo "NODE1 = $NODE1"
+echo "NODE2 = $NODE2"
+echo "NODE3 = $NODE3"
 #MASTER=$(~/environment/ip | awk -Fv '/vm_0/{print $1}')
 #NODE1=$(~/environment/ip | awk -Fv '/vm_1/{print $1}')
 #NODE2=$(~/environment/ip | awk -Fv '/vm_2/{print $1}')
