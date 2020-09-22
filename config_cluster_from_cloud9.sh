a73654e278c3ebfeab9bc5433720573665b406ac
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-09-22 13:38:29 -0300

    Update config_cluster_from_cloud9.sh

--- config_cluster_from_cloud9.sh
+++ config_cluster_from_cloud9.sh
@@ -25,7 +25,7 @@ echo "NODE3 = $NODE3"
 # CONFIGURANDO O MASTER utilizando o KUBEADM INIT:
 echo "sudo hostnamectl set-hostname master" >> master.sh
 echo "Aguardando instalação do KUBEADM."
-ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$MASTER 'while [ $(dpkg -l | grep kubeadm | wc -l) != '1' ]; do { printf .; sleep 1; } done'
+ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$MASTER "while [ $(dpkg -l | grep kubeadm | wc -l) != '1' ]; do { printf .; sleep 1; } done"
 #echo "while [ \$(dpkg -l | grep kubeadm | wc -l) != '1' ]; do { printf .; sleep 1; } done" >> master.sh
 echo "kubeadm version" >> master.sh
 echo "sudo kubeadm config images pull" >> master.sh
