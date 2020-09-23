14cf56d0e0b52c8def4eeb16e01fa5e11cd65034
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-09-23 15:08:51 -0300

    Update config_shutdown_node.sh

--- config_shutdown_node.sh
+++ config_shutdown_node.sh
@@ -6,12 +6,12 @@ NODE2=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_2/) ) { print
 NODE3=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_3/) ) { print $1} }')
 echo "IPs configurados :"
 echo "MASTER = $MASTER"
-echo "NODE1 = $NODE1"
-echo "NODE2 = $NODE2"
-echo "NODE3 = $NODE3"
+echo "WORKER1 = $NODE1"
+echo "WORKER2 = $NODE2"
+echo "WORKER3 = $NODE3"
 
 printf "\n\n"
-echo "Digite o qual node deseja desligar: (ex: worker2)"
+echo "Digite o IP do node que deseja desligar: "
 read NODE
 
 ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$NODE "sudo shutdown -h now"
