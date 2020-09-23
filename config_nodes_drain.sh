6e72e66766fec34d532a8a43e71061ab9ef051f3
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-09-23 09:27:29 -0300

    Update config_nodes_drain.sh

--- config_nodes_drain.sh
+++ config_nodes_drain.sh
@@ -3,6 +3,7 @@
 # outro script: https://github.com/mattmattox/drain-node-on-crash
 # info: https://stackoverflow.com/questions/53641252/kubernetes-recreate-pod-if-node-becomes-offline-timeout/57805955#57805955
 # info: https://dev.to/duske/how-kubernetes-handles-offline-nodes-53b5
+# info: https://kubernetes.io/docs/concepts/scheduling-eviction/taint-and-toleration/#taint-based-evictions
 
 KUBECTL="/usr/local/bin/kubectl"
 
