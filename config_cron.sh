e9ddf9eead305478159c1dc3dcb44f9906ecd285
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-09-23 14:54:11 -0300

    Update config_cron.sh

--- config_cron.sh
+++ config_cron.sh
@@ -4,16 +4,16 @@
 cat >> /home/ubuntu/config_nodes_drain.sh << EOL
 NOT_READY_NODES=\$(kubectl get nodes | grep -P 'NotReady(?!,SchedulingDisabled)' | awk '{print $1}' | xargs echo)
 READY_NODES=\$(kubectl get nodes | grep '\sReady,SchedulingDisabled' | awk '{print $1}' | xargs echo)
-echo "Unready nodes that are undrained: $NOT_READY_NODES"
-echo "Ready nodes: $READY_NODES"
-for node in $NOT_READY_NODES; do
-  echo "Node $node not drained yet, draining..."
-  kubectl drain --ignore-daemonsets --force $node
+echo "Unready nodes that are undrained: \$NOT_READY_NODES"
+echo "Ready nodes: \$READY_NODES"
+for node in \$NOT_READY_NODES; do
+  echo "Node \$node not drained yet, draining..."
+  kubectl drain --ignore-daemonsets --force \$node
   echo "Done"
 done;
-for node in $READY_NODES; do
-  echo "Node $node still drained, uncordoning..."
-  kubectl uncordon $node
+for node in \$READY_NODES; do
+  echo "Node \$node still drained, uncordoning..."
+  kubectl uncordon \$node
   echo "Done"
 done;
 EOL
