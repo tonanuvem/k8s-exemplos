a239406faa8d682fd38822ded5aa57f4c95eca96
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-09-23 14:01:24 -0300

    Update config_cron.sh

--- config_cron.sh
+++ config_cron.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #wget https://tonanuvem.github.io/k8s-exemplos/config_nodes_drain.sh
 croncmd="/home/ubuntu/k8s-exemplos/config_nodes_drain.sh"
+cronlog="/home/ubuntu/config_nodes_drain.log"
 echo $croncmd
-cronjob="*/1 * * * * $croncmd"
+cronjob="*/1 * * * * $croncmd > $cronlog 2>&1"
 ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
 crontab -l
