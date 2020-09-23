3c49159db684275a87a94ec48cf0d0be3b55ac45
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-09-23 14:16:21 -0300

    Update config_cron.sh

--- config_cron.sh
+++ config_cron.sh
@@ -2,19 +2,18 @@
 
 # script para colocar o node que falhou/retornou como CORDON / UNCORDON
 cat >> /home/ubuntu/config_nodes_drain.sh << EOL
-KUBECTL="kubectl"
-NOT_READY_NODES=$($KUBECTL get nodes | grep -P 'NotReady(?!,SchedulingDisabled)' | awk '{print $1}' | xargs echo)
-READY_NODES=$($KUBECTL get nodes | grep '\sReady,SchedulingDisabled' | awk '{print $1}' | xargs echo)
+NOT_READY_NODES=$(kubectl get nodes | grep -P 'NotReady(?!,SchedulingDisabled)' | awk '{print $1}' | xargs echo)
+READY_NODES=$(kubectl get nodes | grep '\sReady,SchedulingDisabled' | awk '{print $1}' | xargs echo)
 echo "Unready nodes that are undrained: $NOT_READY_NODES"
 echo "Ready nodes: $READY_NODES"
 for node in $NOT_READY_NODES; do
   echo "Node $node not drained yet, draining..."
-  $KUBECTL drain --ignore-daemonsets --force $node
+  kubectl drain --ignore-daemonsets --force $node
   echo "Done"
 done;
 for node in $READY_NODES; do
   echo "Node $node still drained, uncordoning..."
-  $KUBECTL uncordon $node
+  kubectl uncordon $node
   echo "Done"
 done;
 EOL
