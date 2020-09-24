a71fad12915c3d929bd9397728e5cede4773e7a6
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-09-24 11:19:33 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,27 +1,36 @@
+# wget https://tonanuvem.github.io/k8s-exemplos/run.sh
 # Escolher se quer executar no modo NODEPORT ou LOADBALANCER
 echo "Digite 1 ou 2 para escolher se quer executar no modo NODEPORT ou LOADBALANCE:"
 echo "1) NODEPORT"  
 echo "2) LOADBALANCE"
+echo "3) EXEMPLOS COM VOLUME PORTWORX"
 read MODO
 if [[ "$MODO" -eq 1 ]]; then
     echo "NODEPORT:"
     # Executar stack modo NODEPORT:
-    kubectl create -f microservice-demo-weaveworks-socks.yaml
+    kubectl create -f https://tonanuvem.github.io/k8s-exemplos/microservice-demo-weaveworks-socks.yaml
     kubectl get svc -n sock-shop
-    kubectl create -f chat_deploy_svc.yml
+    kubectl create -f https://tonanuvem.github.io/k8s-exemplos/chat_deploy_svc.yml
     kubectl get svc -n fiap-chat
-    kubectl create -f deploy_fiap.yml
-    kubectl create -f svc_fiap.yml
+    kubectl create -f https://tonanuvem.github.io/k8s-exemplos/deploy_fiap.yml
+    kubectl create -f https://tonanuvem.github.io/k8s-exemplos/svc_fiap.yml
     kubectl get svc -n fiap-service  
 elif [[ "$MODO" -eq 2 ]]; then
     echo "LOADBALANCE:"
     # Executar stack modo LOADBALANCE:
-    kubectl create -f microservice-demo-weaveworks-socks.yaml
+    kubectl create -f https://tonanuvem.github.io/k8s-exemplos/microservice-demo-weaveworks-socks.yaml
     kubectl get svc -n sock-shop
-    kubectl create -f chat_deploy_svc.yml
+    kubectl create -f https://tonanuvem.github.io/k8s-exemplos/chat_deploy_svc.yml
     kubectl get svc -n fiap-chat
-    kubectl create -f deploy_fiap.yml
-    kubectl create -f svc_fiap_gcp.yml
+    kubectl create -f https://tonanuvem.github.io/k8s-exemplos/deploy_fiap.yml
+    kubectl create -f https://tonanuvem.github.io/k8s-exemplos/svc_fiap_gcp.yml
     kubectl get svc -n fiap-service      
-else echo "Voce precisa escolher 1 ou 2."
+elif [[ "$MODO" -eq 3 ]]; then
+    echo "EXEMPLOS COM VOLUME PORTWORX: WORDPRESS & CLIENTES"
+    # Executar stacks modo VOLUME PORTWOR:
+    kubectl create -f https://tonanuvem.github.io/k8s-exemplos/wordpress_mysql.yaml
+    kubectl create -f https://tonanuvem.github.io/k8s-clientes-nginx-mongodb/vol_mysql.yaml
+    kubectl create -f https://tonanuvem.github.io/k8s-clientes-nginx-mongodb/vol_clientes.yaml
+    kubectl get svc
+else echo "Voce precisa escolher 1 , 2 ou 3."
 fi
