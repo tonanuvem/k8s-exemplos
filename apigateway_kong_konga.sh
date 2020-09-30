7862ab2abc427fcf7fc307c790ad67ce098f2fc9
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-09-30 12:16:02 -0300

    Update apigateway_kong_konga.sh

--- apigateway_kong_konga.sh
+++ apigateway_kong_konga.sh
@@ -1,3 +1,4 @@
+# configurar kong e konga
 cd ~
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
@@ -8,3 +9,11 @@ git clone https://github.com/pantsel/konga.git && cd konga/charts/konga/
 helm install konga -f ./values.yaml ../konga --set service.type=NodePort --namespace kong --wait
 kubectl get svc konga -n kong
 cd ~
+export IP=$(curl -s checkip.amazonaws.com)
+export PORT=$(kubectl -n kong get service kong -o jsonpath='{.spec.ports[?(@.name=="http-admin")].nodePort}')
+
+# habilitar dashboard : https://kubernetes.io/docs/tasks/access-application-cluster/web-ui-dashboard/
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
+
+# configurar rota para o dashboard
+curl -i -X POST --url http://$IP:$PORT/services/ --data 'name=dash' --data 'url=https://kubernetes-dashboard.kubernetes-dashboard.svc.cluster.local'
