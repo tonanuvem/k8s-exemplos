e2564259a1b61df67b23934ae270acf223e54522
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-10-02 14:32:25 -0300

    Update and rename kafka_zookeeper_install.sh to helm_kafka_zookeeper_install.sh

--- helm_kafka_zookeeper_install.sh
+++ helm_kafka_zookeeper_install.sh
@@ -1,10 +1,22 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm install zookeeper bitnami/zookeeper --set replicaCount=3  --set auth.enabled=false --set allowAnonymousLogin=true
+printf "\n\n"
+echo "    Stateful Sets:"
 kubectl get sts
+printf "\n\n"
+echo "    Pods:"
 kubectl get pod
-helm list
+printf "\n\n"
 helm install kafka bitnami/kafka --set zookeeper.enabled=false --set replicaCount=3 --set externalZookeeper.servers=zookeeper.default.svc.cluster.local
+printf "\n\n"
+echo "    Stateful Sets:"
 kubectl get sts
+printf "\n\n"
+echo "    Pods:"
 kubectl get pod
-helm list
+printf "\n\n"
+echo "    Volumes:"
 kubectl get pv
+printf "\n\n"
+echo "    HELM:"
+helm list
