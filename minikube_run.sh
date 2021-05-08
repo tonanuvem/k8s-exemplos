b51ccf789733d4794aea483aa023b6cdd9d253ab
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-05-08 10:17:30 -0300

    Update minikube_run.sh

--- minikube_run.sh
+++ minikube_run.sh
@@ -1,1 +1,5 @@
 sudo minikube start --vm-driver=none --kubernetes-version=v1.18.2 --apiserver-ips=$(curl checkip.amazonaws.com)
+minikube addons enable metrics-server
+minikube dashboard --url &
+kubectl get pod -n kubernetes-dashboard
+kubectl patch svc kubernetes-dashboard -n kubernetes-dashboard -p '{"spec": {"type": "NodePort"}}' && kubectl get svc kubernetes-dashboard -n kubernetes-dashboard
