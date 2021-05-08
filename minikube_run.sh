2fb910bff2375532eefa6ea699434bd09b6a1738
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-05-08 10:29:50 -0300

    Update minikube_run.sh

--- minikube_run.sh
+++ minikube_run.sh
@@ -3,3 +3,6 @@ minikube addons enable metrics-server
 minikube dashboard --url &
 kubectl get pod -n kubernetes-dashboard
 kubectl patch svc kubernetes-dashboard -n kubernetes-dashboard -p '{"spec": {"type": "NodePort"}}' && kubectl get svc kubernetes-dashboard -n kubernetes-dashboard
+export INGRESS_HOST=$(curl -s checkip.amazonaws.com)
+export INGRESS_PORT=$(kubectl -n kubernetes-dashboard get service kubernetes-dashboard -o jsonpath='{.spec.ports[?()].nodePort}')
+echo "Acessar: http://$INGRESS_HOST:$INGRESS_PORT"
