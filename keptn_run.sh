d27893e66b2ff3565dbc8b4381214437aedaa77f
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-05-16 21:35:27 -0300

    Update keptn_run.sh

--- keptn_run.sh
+++ keptn_run.sh
@@ -1,5 +1,7 @@
 # pre-req : cluster + istio
-# minikube addons enable ingress
+# minikube addons enable ingress ingress-dns
+
+# https://tutorials.keptn.sh/tutorials/keptn-full-tour-prometheus-08/index.html?index=..%2F..index#12
 
 # instalar cli do keptn
 curl -sL https://get.keptn.sh | KEPTN_VERSION=0.8.2 bash
@@ -67,3 +69,38 @@ read ENTER
 keptn trigger delivery --project=sockshop --service=carts-db --image=docker.io/mongo --tag=4.2.2 --sequence=delivery-direct
 # Deploy the carts service by specifying the built artifact, which is stored on DockerHub and tagged with version 0.12.1:
 keptn trigger delivery --project=sockshop --service=carts --image=docker.io/keptnexamples/carts --tag=0.12.1
+# Configurar NodePort 
+echo ""
+kubectl get pods --all-namespaces | grep carts-
+echo ""
+echo "sockshop-dev : "
+kubectl patch svc carts -n sockshop-dev -p '{"spec": {"type": "NodePort"}}' && kubectl get svc carts -n sockshop-dev
+echo "sockshop-staging : "
+kubectl patch svc carts -n sockshop-staging -p '{"spec": {"type": "NodePort"}}' && kubectl get svc carts -n sockshop-staging
+echo "sockshop-production : "
+kubectl patch svc carts -n sockshop-production -p '{"spec": {"type": "NodePort"}}' && kubectl get svc carts -n sockshop-production
+echo ""
+echo "\t\t Acessar os ambientes de DEV, STAGING e PROD. Digite ENTER para continuar com monitoração via PROMETHEUS"
+read ENTER
+
+# Generate traffic & Prometheus Monitoring
+cd ../load-generation/cartsloadgen
+# Now let us deploy a pod that will generate some traffic for all three stages of our demo environment.
+kubectl apply -f deploy/cartsloadgen-base.yaml 
+# Optionally, you can verify that the load generator has been started.
+kubectl get pods -n loadgen
+# Setup Prometheus Monitoring
+kubectl apply -f https://raw.githubusercontent.com/keptn-contrib/prometheus-service/release-0.4.0/deploy/service.yaml
+# You can setup Prometheus monitoring and configure scrape jobs using the Keptn CLI. To install the prometheus-service, execute:
+keptn configure monitoring prometheus --project=sockshop --service=carts
+# To verify that the Prometheus scrape jobs are correctly set up, you can access Prometheus by enabling port-forwarding for the prometheus-service:
+#kubectl port-forward svc/prometheus-service 8080 -n monitoring
+
+# Acessar o PROMETHEUS
+kubectl patch svc prometheus-service -n keptn -p '{"spec": {"type": "NodePort"}}' && kubectl get svc prometheus-service -n keptn
+PROM_PORT=$(kubectl get svc prometheus-service -n keptn -o jsonpath='{.spec.ports.nodePort}')
+PROM_ENDPOINT=http://${EXTERNAL_NODE_IP}:${PROM_PORT}/
+echo "Acessar o Swagger (Openapi) $PROM_ENDPOINT"
+echo ""
+echo "\t\t Digite ENTER para continuar com o DEPLOY de um projeto de testes"
+read ENTER
