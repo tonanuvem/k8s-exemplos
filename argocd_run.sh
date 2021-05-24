d5001a9197a339a6bf97b78fa70b69f9539d5946
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-05-24 18:32:11 -0300

    Update argocd_run.sh

--- argocd_run.sh
+++ argocd_run.sh
@@ -1,5 +1,6 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+wget https://raw.githubusercontent.com/claudioed/observability-ops/master/argo-rollout/argo-rollout.sh | sh
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 echo ""
 echo ""
