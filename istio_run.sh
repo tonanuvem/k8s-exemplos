12959d650915028abc8cceb4675913964edc53e0
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-05-24 17:36:03 -0300

    Update istio_run.sh

--- istio_run.sh
+++ istio_run.sh
@@ -9,3 +9,12 @@ kubectl get pod -n istio-system
 kubectl patch svc kiali -n istio-system -p '{"spec": {"type": "NodePort"}}' && kubectl get svc kiali -n istio-system
 kubectl patch svc prometheus -n istio-system -p '{"spec": {"type": "NodePort"}}' && kubectl get svc prometheus -n istio-system
 kubectl patch svc grafana -n istio-system -p '{"spec": {"type": "NodePort"}}' && kubectl get svc grafana -n istio-system
+# Acesso ao Kiali
+export INGRESS_HOST=$(curl -s checkip.amazonaws.com)
+export INGRESS_PORT=$(kubectl -n istio-system get service kiali -o jsonpath='{.spec.ports[?()].nodePort}')
+echo "Acessar Istio Kiali: http://$INGRESS_HOST:$INGRESS_PORT"
+echo ""
+# Acesso ao Grafana
+export INGRESS_PORT=$(kubectl -n istio-system get service grafana -o jsonpath='{.spec.ports[?()].nodePort}')
+echo "Acessar Grafana: http://$INGRESS_HOST:$INGRESS_PORT"
+echo ""
