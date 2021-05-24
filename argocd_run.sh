1c5dc9c6a6c18b296540bc8f91d77752e2bfc747
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-05-24 18:39:51 -0300

    Update argocd_run.sh

--- argocd_run.sh
+++ argocd_run.sh
@@ -1,6 +1,15 @@
+# argocd
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 wget https://raw.githubusercontent.com/claudioed/observability-ops/master/argo-rollout/argo-rollout.sh | sh
+# name space
+kubectl apply -f https://raw.githubusercontent.com/claudioed/bets-helm/master/rollout/apps/ns.yaml
+# apps
+kubectl apply -f https://raw.githubusercontent.com/claudioed/bets-helm/master/rollout/apps/players-app.yaml
+kubectl apply -f https://raw.githubusercontent.com/claudioed/bets-helm/master/rollout/apps/matches-app.yaml
+kubectl apply -f https://raw.githubusercontent.com/claudioed/bets-helm/master/rollout/apps/championships-app.yaml
+kubectl apply -f https://raw.githubusercontent.com/claudioed/bets-helm/master/rollout/apps/bets-app.yaml
+# get secret
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 echo ""
 echo ""
