883e7af7f87ca20ef7c8472a104f4ba5752211b1
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-06-01 18:59:48 -0300

    Update argocd_run.sh

--- argocd_run.sh
+++ argocd_run.sh
@@ -11,7 +11,13 @@ kubectl apply -f https://raw.githubusercontent.com/claudioed/bets-helm/master/ro
 kubectl apply -f https://raw.githubusercontent.com/claudioed/bets-helm/master/rollout/apps/matches-app.yaml
 kubectl apply -f https://raw.githubusercontent.com/claudioed/bets-helm/master/rollout/apps/championships-app.yaml
 kubectl apply -f https://raw.githubusercontent.com/claudioed/bets-helm/master/rollout/apps/bets-app.yaml
+# 
+export INGRESS_HOST=$(curl -s checkip.amazonaws.com)
+export INGRESS_PORT=$(kubectl get svc argocd-server -n argocd -o jsonpath='{.spec.ports[?(@.name=="http")].nodePort}')
+echo "Acessar ARGOCD: http://$INGRESS_HOST:$INGRESS_PORT"
+echo ""
 # get secret
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+echo "Solicitar SECRET : kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath=\"{.data.password}\" | base64 -d && echo """
+#kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo ""
 echo ""
 echo ""
