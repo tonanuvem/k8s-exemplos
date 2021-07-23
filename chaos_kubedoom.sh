628a0b62e25f55fae6c0e711b142e13b9a1c10ec
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-07-23 11:40:15 -0300

    Update chaos_kubedoom.sh

--- chaos_kubedoom.sh
+++ chaos_kubedoom.sh
@@ -5,6 +5,32 @@
 sudo apt-get update
 sudo apt-get -y install xfce4 xfce4-goodies tightvncserver xfonts-base
 
+# instalar kubeinvaders
+# https://github.com/lucky-sideburn/KubeInvaders
+
+helm repo add kubeinvaders https://lucky-sideburn.github.io/helm-charts/
+
+kubectl create namespace kubeinvaders
+
+helm install kubeinvaders --set-string target_namespace="namespace1\,namespace2" \
+-n kubeinvaders kubeinvaders/kubeinvaders --set ingress.hostName=kubeinvaders.io --set image.tag=v1.7
+
+# Install KubeInvaders
+# Create a namespace for the game application.
+kubectl create namespace kubeinvaders
+# Grab the source code that contains the Helm chart.
+git clone https://github.com/lucky-sideburn/KubeInvaders && cd KubeInvaders
+# Install the game using the Helm chart.
+helm install kubeinvaders ./helm-charts/kubeinvaders \
+--version 0.1.1 \
+--namespace kubeinvaders \
+--set service.type=NodePort \
+--set ingress.enabled=false \
+--set target_namespace="default\,more-apps" \
+--set route_host=2886795298-30016-jago01.environments.katacoda.com
+# Set NodePort value
+kubectl patch service kubeinvaders -n kubeinvaders --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30016}]'
+
 # instalar Kube Doom
 # https://github.com/Alynder/kubedoom
 # https://opensource.com/article/21/6/kube-doom
@@ -26,5 +52,7 @@ kubectl get pod -n kubedoom
 # Executar interface web novnc para conectar
 # https://github.com/fcwu/docker-ubuntu-vnc-desktop
 
-docker run -p 6080:80 -v /dev/shm:/dev/shm -d dorowu/ubuntu-desktop-lxde-vnc
+docker network create doom
+docker run --name ubt-desk -p 6080:80 -v /dev/shm:/dev/shm -d dorowu/ubuntu-desktop-lxde-vnc
+docker exec -ti ubt-desk "apt update && apt-get install "
 #docker run -p 6080:80 -p 5900:5900 -v /dev/shm:/dev/shm dorowu/ubuntu-desktop-lxde-vnc
