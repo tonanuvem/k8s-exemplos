4b866675b5da40dea5ddc794973d1e2d0e4dd5ef
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-09-28 12:38:52 -0300

    Update microk8s_cloud9.sh

--- microk8s_cloud9.sh
+++ microk8s_cloud9.sh
@@ -10,5 +10,12 @@ microk8s kubectl get nodes
 # configurando addons:
 microk8s enable dns storage dashboard
 
+# K8S Dashboard
+#Get the token for the default user
+token=$(kubectl -n kube-system get secret | grep default-token | cut -d " " -f1)
+kubectl -n kube-system describe secret $token
+#Port forward  the K8S Dashboard port 443 to port 10443 onto the EC2.
+microk8s kubectl port-forward -n kube-system service/kubernetes-dashboard 10443:443 --address 0.0.0.0
+
 # instalando kubeflow:
 microk8s enable kubeflow --bundle lite --password admin
