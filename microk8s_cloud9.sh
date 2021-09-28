7bb1be4e0b72f068b2f34c31ef8243fdce04d566
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-09-28 16:44:58 -0300

    Update microk8s_cloud9.sh

--- microk8s_cloud9.sh
+++ microk8s_cloud9.sh
@@ -8,18 +8,20 @@ sudo microk8s status --wait-ready
 alias kubectl='microk8s kubectl'
 microk8s kubectl get nodes
 
+# setando o dominio
+export INGRESS_HOST=$(curl checkip.amazonaws.com)
+export INGRESS_DOMAIN=${INGRESS_HOST}.nip.io
+echo $INGRESS_DOMAIN
+
 # K8S Dashboard
-#microk8s enable dns storage dashboard
+microk8s enable dns storage dashboard metallb:${INGRESS_HOST}-{$INGRESS_HOST}
 #Get the token for the default user
 #token=$(microk8s kubectl -n kube-system get secret | grep default-token | cut -d " " -f1)
 #microk8s kubectl -n kube-system describe secret $token
 #Port forward  the K8S Dashboard port 443 to port 10443 onto the EC2.
 #microk8s kubectl port-forward -n kube-system service/kubernetes-dashboard 10443:443 --address 0.0.0.0 &
 
-# setando o dominio
-export INGRESS_HOST=$(curl checkip.amazonaws.com)
-export INGRESS_DOMAIN=${INGRESS_HOST}.nip.io
-echo $INGRESS_DOMAIN
+#microk8s enable metallb:$(curl ipinfo.io/ip)-$(curl ipinfo.io/ip)
 
 # instalando kubeflow:
 microk8s enable kubeflow --bundle lite --password admin --hostname ${INGRESS_HOST}
