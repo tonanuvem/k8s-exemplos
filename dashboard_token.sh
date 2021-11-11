88e84343a0fe288b13e2178bbe97209cffd5ddde
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-11-11 15:27:05 -0300

    Update dashboard_token.sh

--- dashboard_token.sh
+++ dashboard_token.sh
@@ -3,6 +3,7 @@ export COLOR_RESET='\e[0m'
 export COLOR_LIGHT_GREEN='\e[0;49;32m' 
 
 export INGRESS_HOST=$(curl -s checkip.amazonaws.com)
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
 kubectl patch svc kubernetes-dashboard -n kubernetes-dashboard -p '{"spec": {"type": "NodePort"}}'
 kubectl get svc kubernetes-dashboard -n kubernetes-dashboard
 export INGRESS_PORT=$(kubectl -n kubernetes-dashboard get service kubernetes-dashboard -o jsonpath='{.spec.ports[?()].nodePort}')
