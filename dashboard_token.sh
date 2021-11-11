d89aa4e1165682aa74433a346e7ca3169a90d5ed
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-11-11 15:23:29 -0300

    Update dashboard_token.sh

--- dashboard_token.sh
+++ dashboard_token.sh
@@ -2,14 +2,14 @@
 export COLOR_RESET='\e[0m'
 export COLOR_LIGHT_GREEN='\e[0;49;32m' 
 
-# Technique to grab Kubernetes dashboard access token.
-# Typically used in Katacoda scenarios.
-
-echo 'To access the dashboard click on the Kubernetes Dashboard tab above this command '
-echo 'line. At the sign in prompt select Token and paste in the token that is shown below.'
-echo ''
-echo 'For Kubernetes clusters exposed to the public, always lock administration access including '
-echo 'access to the dashboard. Why? https://www.wired.com/story/cryptojacking-tesla-amazon-cloud/'
+export INGRESS_HOST=$(curl -s checkip.amazonaws.com)
+kubectl patch svc kubernetes-dashboard -n kubernetes-dashboard -p '{"spec": {"type": "NodePort"}}'
+kubectl get svc kubernetes-dashboard -n kubernetes-dashboard
+export INGRESS_PORT=$(kubectl -n kubernetes-dashboard get service kubernetes-dashboard -o jsonpath='{.spec.ports[?()].nodePort}')
+echo ""
+echo "Acessar K8S Dashboard: http://$INGRESS_HOST:$INGRESS_PORT"
+    
+# Kubernetes dashboard access token.
 
 SECRET_RESOURCE=$(kubectl get secrets -n kube-system -o name | grep dash-kubernetes-dashboard-token)
 ENCODED_TOKEN=$(kubectl get $SECRET_RESOURCE -n kube-system -o=jsonpath='{.data.token}')
