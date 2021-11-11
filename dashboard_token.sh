acb5b36b837fd79ef6dd9ccc7e7167766d1f6661
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-11-11 16:16:11 -0300

    Update dashboard_token.sh

--- dashboard_token.sh
+++ dashboard_token.sh
@@ -3,8 +3,17 @@ export COLOR_RESET='\e[0m'
 export COLOR_LIGHT_GREEN='\e[0;49;32m' 
 
 export INGRESS_HOST=$(curl -s checkip.amazonaws.com)
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
-kubectl patch svc kubernetes-dashboard -n kubernetes-dashboard -p '{"spec": {"type": "NodePort"}}'
+
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
+
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm repo update
+kubectl create namespace kubernetes-dashboard 
+
+# https://github.com/kubernetes/dashboard/blob/master/aio/deploy/helm-chart/kubernetes-dashboard/values.yaml
+helm install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard -n kubernetes-dashboard --set=service.type=NodePort,service.nodePort=32100,protocolHttp=true,metricsScraper.enabled=true,metrics-server.enabled=true,resources.limits.cpu=200m
+
+# kubectl patch svc kubernetes-dashboard -n kubernetes-dashboard -p '{"spec": {"type": "NodePort"}}'
 kubectl get svc kubernetes-dashboard -n kubernetes-dashboard
 export INGRESS_PORT=$(kubectl -n kubernetes-dashboard get service kubernetes-dashboard -o jsonpath='{.spec.ports[?()].nodePort}')
 echo ""
