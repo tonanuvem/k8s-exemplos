353a5895e7aa71665ef312a90f9c8a721f89e993
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-11-12 14:02:14 -0300

    Update download_install_version.sh

--- download_install_version.sh
+++ download_install_version.sh
@@ -5,9 +5,9 @@
 echo "Digite qual uma versão específica (exemplos: 1.17.11 ou 1.20.6)"
 read VERSAO
 
-wget https://storage.googleapis.com/kubernetes-release/release/$VERSAO/bin/linux/amd64/kubectl
-wget https://storage.googleapis.com/kubernetes-release/release/$VERSAO/bin/linux/amd64/kubeadm
-wget https://storage.googleapis.com/kubernetes-release/release/$VERSAO/bin/linux/amd64/kubelet
+wget https://storage.googleapis.com/kubernetes-release/release/v$VERSAO/bin/linux/amd64/kubectl
+wget https://storage.googleapis.com/kubernetes-release/release/v$VERSAO/bin/linux/amd64/kubeadm
+wget https://storage.googleapis.com/kubernetes-release/release/v$VERSAO/bin/linux/amd64/kubelet
 
 chmod +x kubectl
 chmod +x kubeadm
