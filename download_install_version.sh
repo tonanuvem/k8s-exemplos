780423346e1e796424640d02646efb78e6e8aca1
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-11-12 13:55:07 -0300

    Create download_install_version.sh

---
+++ download_install_version.sh
@@ -0,0 +1,16 @@
+# https://cloud.ibm.com/docs/containers?topic=containers-cs_cli_install
+
+echo "Digite qual uma versão específica (exemplos: 1.17.11 ou 1.20.6)"
+READ VERSAO
+
+curl -s "https://storage.googleapis.com/kubernetes-release/release/$VERSAO/bin/linux/amd64/kubectl"
+curl -s "https://storage.googleapis.com/kubernetes-release/release/$VERSAO/bin/linux/amd64/kubeadm"
+curl -s "https://storage.googleapis.com/kubernetes-release/release/$VERSAO/bin/linux/amd64/kubelet"
+
+chmod +x kubectl
+chmod +x kubeadm
+chmod +x kubelet
+
+sudo mv kubectl /usr/bin
+sudo mv kubeadm /usr/bin
+sudo mv kubelet /usr/bin
