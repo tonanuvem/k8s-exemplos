0633e72026f72a710c7df5e05d9609568075b53b
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-03-09 17:04:21 -0300

    Update config_ingress_helm.sh

--- config_ingress_helm.sh
+++ config_ingress_helm.sh
@@ -2,6 +2,7 @@
 
 # script para colocar Ingress Controller NGINX utilizando Helm :
 # https://docs.nginx.com/nginx-ingress-controller/installation/installation-with-helm/
+# https://kubernetes.github.io/ingress-nginx/deploy/
 
 # Helm Repository
 helm repo add nginx-stable https://helm.nginx.com/stable
