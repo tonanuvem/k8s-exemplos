c2bcfb1cbf233dc9d5f68ec2bb98fe3571624e00
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-03-10 16:38:35 -0300

    Update config_ingress_helm.sh

--- config_ingress_helm.sh
+++ config_ingress_helm.sh
@@ -45,5 +45,37 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 helm install ingress-controller ingress-nginx/ingress-nginx --version 4.0.18 --namespace nginx
 
+# Caddy : proxy para as portas 80 e 443:
+
+cat <<EOF | sudo tee Caddyfile
+:80 {
+        # Set this path to your site's directory.
+        root * /usr/share/caddy
+
+        # Enable the static file server.
+        file_server
+
+        # Another common task is to set up a reverse proxy:
+        reverse_proxy localhost:32080
+
+        # Or serve a PHP site through php-fpm:
+        # php_fastcgi localhost:9000
+}
+:443 {
+        # Set this path to your site's directory.
+        root * /usr/share/caddy
+
+        # Enable the static file server.
+        file_server
+
+        # Another common task is to set up a reverse proxy:
+        reverse_proxy localhost:32443
+
+        # Or serve a PHP site through php-fpm:
+        # php_fastcgi localhost:9000
+}
+EOF
+docker run -d -p 80:80 -p 80:80 -v ./Caddyfile:/etc/caddy/Caddyfile caddy
+
 # Lambda Dynamic DNS CONFIG:
 # https://github.com/awslabs/route53-dynamic-dns-with-lambda
