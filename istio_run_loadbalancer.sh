754bcff16df4f50342cbdb2991bacd36b9e7d963
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2023-01-12 11:51:28 -0300

    Update istio_run_loadbalancer.sh

--- istio_run_loadbalancer.sh
+++ istio_run_loadbalancer.sh
@@ -91,17 +91,23 @@ EOF
 #kubectl patch svc grafana -n istio-system -p '{"spec": {"type": "LoadBalancer"}}' && kubectl get svc grafana -n istio-system
 
 # Aguardando os IPs Externos
-echo ""
+#echo ""
 echo "Aguardando os IPs Externos dos serviços (K8S LoadBalancer)"
-while [ $(kubectl get service kiali -n istio-system -o jsonpath='{ .status.loadBalancer.ingress[].ip }'| wc -m) = '0' ]; do { printf .; sleep 1; } done
-export IP_KIALI=$(kubectl get service kiali -n istio-system -o jsonpath='{ .status.loadBalancer.ingress[].ip }')
-echo " IP_externo KIALI OK"
-while [ $(kubectl get service grafana -n istio-system -o jsonpath='{ .status.loadBalancer.ingress[].ip }'| wc -m) = '0' ]; do { printf .; sleep 1; } done
-export IP_GRAFANA=$(kubectl get service grafana -n istio-system -o jsonpath='{ .status.loadBalancer.ingress[].ip }')
-echo " IP_externo GRAFANA OK"
+while [ $(kubectl get service istio-ingressgateway -n istio-system -o jsonpath='{ .status.loadBalancer.ingress[].ip }'| wc -m) = '0' ]; do { printf .; sleep 1; } done
+export IP=$(kubectl get service istio-ingressgateway -n istio-system -o jsonpath='{ .status.loadBalancer.ingress[].ip }')
+
+# Não utilizamos LB
+#while [ $(kubectl get service kiali -n istio-system -o jsonpath='{ .status.loadBalancer.ingress[].ip }'| wc -m) = '0' ]; do { printf .; sleep 1; } done
+#export IP_KIALI=$(kubectl get service kiali -n istio-system -o jsonpath='{ .status.loadBalancer.ingress[].ip }')
+#echo " IP_externo KIALI OK"
+#while [ $(kubectl get service grafana -n istio-system -o jsonpath='{ .status.loadBalancer.ingress[].ip }'| wc -m) = '0' ]; do { printf .; sleep 1; } done
+#export IP_GRAFANA=$(kubectl get service grafana -n istio-system -o jsonpath='{ .status.loadBalancer.ingress[].ip }')
+#echo " IP_externo GRAFANA OK"
+
+#echo ""
 # Acesso ao Kiali
-echo "Acessar Istio Kiali: http://$IP_KIALI"
+echo "Acessar Istio Kiali: http://$IP/kiali"
 echo ""
 # Acesso ao Grafana
-echo "Acessar Grafana: http://$IP_GRAFANA"
+echo "Acessar Grafana: http://$IP/grafana"
 echo ""
